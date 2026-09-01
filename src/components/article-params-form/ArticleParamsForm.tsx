b87d9f62f374791444c660837998f1dccb35442a
EgorDormidontov <113050823+EgorDormidontov@users.noreply.github.com>
2026-09-01 11:57:22 +0300

    commit 1

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,155 @@
+import { FormEvent, useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
+
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	onChange: (state: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({ onChange }: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const rootRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const handleOutsideClick = (event: MouseEvent) => {
+			if (
+				isOpen &&
+				rootRef.current &&
+				!rootRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener('click', handleOutsideClick);
+
+		return () => {
+			document.removeEventListener('click', handleOutsideClick);
+		};
+	}, [isOpen]);
+
+	const handleArrowClick = () => {
+		setIsOpen((prevState) => !prevState);
+	};
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		onChange(formState);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		onChange(defaultArticleState);
+	};
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={rootRef}>
+			<ArrowButton isOpen={isOpen} onClick={handleArrowClick} />
+
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Параметры статьи
+					</Text>
+
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(fontFamilyOption) =>
+							setFormState({
+								...formState,
+								fontFamilyOption,
+							})
+						}
+					/>
+
+					<RadioGroup
+						title='Размер шрифта'
+						name='font-size'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(fontSizeOption) =>
+							setFormState({
+								...formState,
+								fontSizeOption,
+							})
+						}
+					/>
+
+					<Select
+						title='Цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(fontColor) =>
+							setFormState({
+								...formState,
+								fontColor,
+							})
+						}
+					/>
+
+					<Separator />
+
+					<Select
+						title='Цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(backgroundColor) =>
+							setFormState({
+								...formState,
+								backgroundColor,
+							})
+						}
+					/>
+
+					<RadioGroup
+						title='Ширина контента'
+						name='content-width'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(contentWidth) =>
+							setFormState({
+								...formState,
+								contentWidth,
+							})
+						}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
+
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
